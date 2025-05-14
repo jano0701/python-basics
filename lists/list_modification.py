80edc5e137912b5a3497e25f5e89d19185e6e426
Jano <jano2000@live.de>
2025-05-14 15:19:20 +0200

    Initial commit with folder structure

---
+++ lists/list_modification.py
@@ -0,0 +1,4 @@
+names = ["John", "Jane", "Doe", "Alice", "Bob"]
+names[0] = "Jon"
+print(names[0:3])
+print(names)
---
+++ print_statements/sum_two_numbers.py
@@ -0,0 +1,4 @@
+first = input("Enter the first number: ")
+second = input("Enter the second number: ")
+sum = int(first) + int(second)
+print("The sum of", first, "and", second, "is:", sum)
